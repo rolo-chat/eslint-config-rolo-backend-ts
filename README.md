e157d9c433166bf40ba8da1f47172cd4c640cc6d
Noe Charmet <noe@rolo.chat>
2022-01-04 10:18:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# eslint-config-rolo-backend-ts
+# Eslint typescript config Rolo backend
+
+Typescript Eslint config for the backend teams working on Rolo.
+
+## Install
+
+```bash
+npm i -D eslint-config-rolo-backend-ts eslint
+```
+
+Add it as a config in `.eslintrc`
+
+```json
+{
+  "extends": ["eslint-config-rolo-backend-ts"]
+}
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    node: true,
+  },
+  parserOptions: { project: ["./tsconfig.json"] },
+  plugins: ["@typescript-eslint", "jsdoc", "prettier"],
+  extends: [
+    "google",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "valid-jsdoc": "off",
+    "require-jsdoc": "off",
+    "jsdoc/require-jsdoc": [
+      "warn",
+      {
+        contexts: [
+          { context: "ClassDeclaration", inlineCommentBlock: true },
+          { context: "ClassProperty", inlineCommentBlock: true },
+          { context: "PropertyDefinition", inlineCommentBlock: true },
+          { context: "FunctionDeclaration", inlineCommentBlock: true },
+          { context: "MethodDefinition", inlineCommentBlock: true },
+          { context: "TSPropertySignature", inlineCommentBlock: true },
+        ],
+      },
+    ],
+    "import/order": [
+      "warn",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "@typescript-eslint/explicit-member-accessibility": "warn",
+    "@typescript-eslint/member-ordering": "warn",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    jsdoc: {
+      mode: "typescript",
+    },
+  },
+};
